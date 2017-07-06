2e053dc639c6e4e51d8b980f1374631fdc63ecc6
Cathy Dutton <cathy@Cathys-MacBook-Pro.local>
2017-07-06 11:14:50 +0100

    Update content on junior, length and under 12 screens

--- server/routes/buy/licence-length.js
+++ server/routes/buy/licence-length.js
@@ -2,29 +2,33 @@ const handlers = {
   get: function (request, reply) {
 
     // Set global user variable
-    if(!global.users) {
+    if (!global.users) {
       global.users = []
     }
 
     return reply.view('licence-length', {
-      pageTitle: 'What do you want to do?',
+      //pageTitle: 'What do you want to do?',
+      pageTitle: 'Which fishing licence do you want to get?',
       errorMessage: 'Tell us what you want to do',
       items: {
         one: {
-          text: 'Buy a new 12-month licence',
+          //text: 'Buy a new 12-month licence',
+          text: '12-month licence',
           name: 'licence_length',
           id: '12-months',
           value: '12-months',
           selectedText: '12-month licences are now valid for 365 days from their start date and can be purchased at any time during the year.'
         },
         two: {
-          text: 'Buy a new 8-day licence',
+          //text: 'Buy a new 8-day licence',
+          text: '8-day licence',
           name: 'licence_length',
           id: '8-days',
           value: '8-days',
         },
         three: {
-          text: 'Buy a new 1-day licence',
+          //text: 'Buy a new 1-day licence',
+          text: '1-day licence',
           name: 'licence_length',
           id: '1-day',
           value: '1-day',
@@ -34,36 +38,36 @@ const handlers = {
     })
   },
   post: function (request, reply) {
-      returnURL = request.query.returnUrl
-      request.session.licenceLength = request.payload.licence_length
-      if (returnURL) {
-          if (request.session.licenceLength === '12-months') {
-            return reply.redirect('disability?returnUrl=/buy/summary')
-          } else if (request.session.licenceLength === '8-days' || request.session.licenceLength === '1-day') {
-            request.session.isFull = false
-            request.session.haveTime = false
-            return reply.redirect('licence-start-time?returnUrl=/buy/summary')
-          } else {
-            return reply.redirect(returnURL)
-          }
+    returnURL = request.query.returnUrl
+    request.session.licenceLength = request.payload.licence_length
+    if (returnURL) {
+      if (request.session.licenceLength === '12-months') {
+        return reply.redirect('disability?returnUrl=/buy/summary')
+      } else if (request.session.licenceLength === '8-days' || request.session.licenceLength === '1-day') {
+        request.session.isFull = false
+        request.session.haveTime = false
+        return reply.redirect('licence-start-time?returnUrl=/buy/summary')
       } else {
-        if (request.session.licenceLength === '12-months' || request.session.licenceLength === '12 months') {
-          request.session.isFull = true
-          request.session.haveTime = true
-          request.session.licenceLength = '12-months'
-          return reply.redirect('name')
-        } else if (request.session.licenceLength === '8-days' || request.session.licenceLength === '8 days') {
-          request.session.isFull = false
-          request.session.haveTime = false
-          request.session.licenceLength = '8-days'
-          return reply.redirect('name')
-        } else if (request.session.licenceLength === '1-day' || request.session.licenceLength === '1 day') {
-          request.session.isFull = false
-          request.session.haveTime = false
-          request.session.licenceLength = '1-day'
-          return reply.redirect('name')
-        }
+        return reply.redirect(returnURL)
       }
+    } else {
+      if (request.session.licenceLength === '12-months' || request.session.licenceLength === '12 months') {
+        request.session.isFull = true
+        request.session.haveTime = true
+        request.session.licenceLength = '12-months'
+        return reply.redirect('name')
+      } else if (request.session.licenceLength === '8-days' || request.session.licenceLength === '8 days') {
+        request.session.isFull = false
+        request.session.haveTime = false
+        request.session.licenceLength = '8-days'
+        return reply.redirect('name')
+      } else if (request.session.licenceLength === '1-day' || request.session.licenceLength === '1 day') {
+        request.session.isFull = false
+        request.session.haveTime = false
+        request.session.licenceLength = '1-day'
+        return reply.redirect('name')
+      }
+    }
 
 
 
